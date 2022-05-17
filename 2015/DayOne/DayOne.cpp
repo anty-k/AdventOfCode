760b3faa0a08c35f2409d5852e6eb8b534b5a898
Quentin Dejour <quentin.dejour@gmail.com>
2022-05-16 21:25:16 -0400

    Restructure project & add 2015 day 1

---
+++ 2015/DayOne/DayOne.cpp
@@ -0,0 +1,58 @@
+// DayOne.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void takeStairs(char instruction, int &stair)
+{
+	if (instruction == '(')
+	{
+		stair++;
+	}
+	else if (instruction == ')')
+	{
+		stair--;
+	}
+}
+
+int main()
+{
+	string filename("input.txt");
+	char byte;
+	int stair = 0;
+	int basementInstruction;
+	bool basementReached = false;
+
+	ifstream input_file(filename);
+
+	if (!input_file.is_open())
+	{
+		cout << "Unable to open file";
+		return 0;
+	}
+	
+	int i = 0;
+	while (input_file.get(byte))
+	{
+		takeStairs(byte, stair);
+		i++;
+
+		if (stair == -1 && !basementReached)
+		{
+			basementReached = true;
+			basementInstruction = i;
+		}
+	}
+
+	input_file.close();
+
+	cout << "Santa, go to the " << stair << " floor!" << endl;
+	cout << "First basement floor reached at the " << basementInstruction << "th instruction" << endl;
+
+	return 0;
+}
